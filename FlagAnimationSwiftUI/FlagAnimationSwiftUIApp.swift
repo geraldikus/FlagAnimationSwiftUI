cdc798c6b3d6ed97a0a7fccbfb772782206cec85
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2024-03-28 12:28:11 +0400

    Initial commit

---
+++ FlagAnimationSwiftUI/FlagAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagAnimationSwiftUIApp.swift
+//  FlagAnimationSwiftUI
+//
+//  Created by Anton on 28.03.24.
+//
+
+import SwiftUI
+
+@main
+struct FlagAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
